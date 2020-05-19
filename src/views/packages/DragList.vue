42ba29438973aafd8291ede971d6ad8a3a1d1a41
LoveHins <354372738@qq.com>
2020-05-19 19:18:01 +0800

    拖拽列表功能优化

--- src/views/packages/DragList.vue
+++ src/views/packages/DragList.vue
@@ -3,19 +3,15 @@
     <el-row :gutter="20">
       <el-col :span="5">
         <div class="list-title">列表1事项</div>
-        <div class="list-cont">
-          <Draggable v-model="listOne" :move="onMove" group="list" @end="handleChange($event)">
-            <el-card v-for="(item, index) in listOne" :key="index" shadow="hover">{{ item.name }}</el-card>
-          </Draggable>
-        </div>
+        <Draggable v-model="listOne" :move="onMove" group="list" @end="handleChange($event, '列表1')" class="list-cont">
+          <el-card v-for="(item, index) in listOne" :key="index" shadow="hover">{{ item.name }}</el-card>
+        </Draggable>
       </el-col>
       <el-col :span="5">
         <div class="list-title">列表2事项</div>
-        <div class="list-cont">
-          <Draggable v-model="listTwo" :move="onMove" group="list" @end="handleChange($event)">
-            <el-card v-for="(item, index) in listTwo" :key="index" shadow="hover">{{ item.name }}</el-card>
-          </Draggable>
-        </div>
+        <Draggable v-model="listTwo" :move="onMove" group="list" @end="handleChange($event, '列表2')" class="list-cont">
+          <el-card v-for="(item, index) in listTwo" :key="index" shadow="hover">{{ item.name }}</el-card>
+        </Draggable>
       </el-col>
       <el-col :span="4">
         <div class="list-title">操作记录</div>
@@ -23,11 +19,11 @@
       </el-col>
       <el-col :span="5">
         <div class="list-title">列表1数据</div>
-        <pre>{{ listOne }}</pre>
+        <pre class="list-cont">{{ listOne }}</pre>
       </el-col>
       <el-col :span="5">
         <div class="list-title">列表2数据</div>
-        <pre>{{ listTwo }}</pre>
+        <pre class="list-cont">{{ listTwo }}</pre>
       </el-col>
     </el-row>
   </div>
@@ -37,14 +33,13 @@
 import Draggable from 'vuedraggable'
 import Hints from '../../components/Hints'
 const message = [
-  'vue.draggable',
-  'draggable',
-  'component',
-  'for',
-  'vue.js 2.0',
-  'based',
-  'on',
-  'Sortablejs'
+  'ECMAScript6',
+  'VueJS',
+  'ReactJS',
+  'AngularJS',
+  'Webpack',
+  'NodeJS',
+  'TypeScript'
 ]
 
 export default {
@@ -63,14 +58,60 @@ export default {
     onMove (obj) {
       console.log(obj)
     },
-    handleChange (event) {
+    handleChange (event, type) {
       console.log(event)
+      const srcClassName = (event.srcElement || event.target).classList[0]
+      const targetClassName = event.to.classList[0]
+      let src = ''
+      let target = ''
+      if (srcClassName === targetClassName) {
+        if (type === '列表1') {
+          src = '列表1'
+          target = '列表1'
+        } else {
+          src = '列表2'
+          target = '列表2'
+        }
+      } else {
+        if (type === '列表1') {
+          src = '列表1'
+          target = '列表2'
+        } else {
+          src = '列表2'
+          target = '列表1'
+        }
+      }
+      console.log(src, target)
       // this.handleList.push(`${src} => ${target}, ${oldIndex} => ${newIndex}`)
     }
   }
 }
 </script>
 
-<style scoped>
-
+<style lang="less">
+.drag-list-wrapper{
+  .list-title{
+    height: 40px;
+    margin-bottom: 8px;
+    line-height: 40px;
+    font-size: 15px;
+    font-weight: 600;
+    text-align: center;
+    box-shadow: rgba(0, 0, 0, 0.1) 0 2px 12px 0;
+  }
+  .list-cont{
+    height: 510px;
+    padding: 10px;
+    box-shadow: rgba(0, 0, 0, 0.1) 0 2px 12px 0;
+    .el-card{
+      margin-bottom: 10px;
+      .el-card__body{
+        padding: 14px 20px;
+      }
+    }
+  }
+  pre{
+    font-family: Consolas,Menlo,Courier,monospace;
+  }
+}
 </style>
