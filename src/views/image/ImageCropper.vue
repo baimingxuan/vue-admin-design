d9e44bb8ee1af53a7b811afcc4c6873296026ea4
LoveHins <354372738@qq.com>
2020-07-06 18:08:04 +0800

    新增图片裁剪组件

--- src/views/image/ImageCropper.vue
+++ src/views/image/ImageCropper.vue
@@ -1,10 +1,35 @@
 <template>
-  <div class="image-cropper-wrapper"></div>
+  <div class="image-cropper-wrapper">
+    <Hints>
+      <template slot="hintName">图片裁剪Vue-Cropper</template>
+      <template slot="hintInfo">
+        <p>Vue-Cropper：一个优雅的图片裁剪插件，可实现裁剪、上传、下载功能，并支持下载PNG、JPG、WEBP等图片格式</p>
+        <p>github地址：访问 <el-link type="success" href="https://github.com/xyxiao001/vue-cropper" target="_blank">Vue-Cropper</el-link></p>
+      </template>
+    </Hints>
+    <VueCropper
+      ref="cropper"
+      :img="configObj.imgSrc"
+      :outputSize="configObj.size"
+      :outputType="configObj.outputType"
+    ></VueCropper>
+  </div>
 </template>
 
 <script>
+import VueCropper  from 'vue-cropper'
+import Hints from '../../components/Hints/index'
+
 export default {
-  name: 'ImageCropper'
+  name: 'ImageCropper',
+  data () {
+    return {
+      configObj: {
+        imgSrc: ''
+      }
+    }
+  },
+  components: { Hints, VueCropper }
 }
 </script>
 
