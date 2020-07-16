d32dba0a803a2cdffcf33198c4f55d42f85eddfc
baimingxuan <354372738@qq.com>
2020-07-16 19:08:02 +0800

    解决打包请求数据丢失bug

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 export function getTableList (param) {
   return axios({
-    url: '../../static/mock-data/table-list.json',
+    url: './static/mock-data/table-list.json',
     method: 'get',
     param
   })
