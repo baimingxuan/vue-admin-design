fe037512dbfc9ad6915bdd579b3f69b45b93e48e
baimingxuan <354372738@qq.com>
2020-11-06 11:42:00 +0800

    生产环境配置Gzip压缩

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,6 @@
 'use strict'
 
-const name = 'vue-admin-design' // 网页标题title
-const port = 8000 // 端口号
+const CompressionWebpackPlugin = require('compression-webpack-plugin')
 
 module.exports = {
   publicPath: './', // 部署应用包时的基本 url
@@ -10,12 +9,9 @@ module.exports = {
   lintOnSave: process.env.NODE_ENV === 'development', // 仅在开发模式下进行 eslint 检测代码
   productionSourceMap: false, // 禁用生产环境的 source map
   runtimeCompiler: true, // 是否运行时组件中使用 template
-  configureWebpack: {
-    name: name // 配置网页title 名称
-  },
   devServer: {
     host: '0.0.0.0', // 默认是 localhost,可不配置
-    port: port, // 配置端口号
+    port: 8000, // 配置端口号
     open: true, // 启动是否打开浏览器
     overlay: { // 是否在浏览器上显示编译的 errors 或 warnings
       warnings: false,
@@ -29,6 +25,21 @@ module.exports = {
           '^/': ''
         }
       }
+    }
+  },
+  configureWebpack(config) {
+    // 生产环境配置 Gzip 压缩
+    if (process.env.NODE_ENV === 'production') {
+      config.plugins.push(
+        new CompressionWebpackPlugin({
+          // 正则匹配需要压缩的文件后缀
+          test: /\.(js|css|svg|woff|ttf|json|html)$/,
+          // 大于20kb的会压缩
+          threshold: 20480,
+          // 是否删除原文件
+          deleteOriginalAssets: false
+        })
+      )
     }
   }
 }
