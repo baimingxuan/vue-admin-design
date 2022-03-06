72d547e34d984d81f6fbe51e4f7970bc6c42529c
baimingxuan <354372738@qq.com>
2022-03-06 23:18:35 +0800

    feat: add SectionMain component

---
+++ src/components/SectionMain/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="section-main-wrapper" />
+</template>
+
+<script>
+export default {
+  name: 'SectionMain'
+}
+</script>
+
+<style lang="less">
+.section-main-wrapper {}
+</style>
